751e8565a0d4bb1ccf9a9aad46c897e77164e703
“OdinsBeard82 <danielcharles_500@hotmail.com>
2023-04-11 12:29:01 +0100

    assignment part2 done

--- script.js
+++ script.js
@@ -1,103 +1,26 @@
-const book = {
-  title: 'Interview with a Vampire',
-  author: 'Anne Rice',
-  pages: '371',
-  read: 'yes'
-
-};
-
-const book2 = {
-  title: 'The Vampire Lestat',
-  author: 'Anne Rice',
-  pages: '560',
-  read: 'yes'
-};
-
-const book3 = {
-  title: 'The Vampire Armand',
-  author: 'Anne Rice',
-  pages: '387',
-  read: 'yes'
-};
-
-const book4 = {
-  title: 'Memnoch the Devil',
-  author: 'Anne Rice',
-  pages: '354',
-  read: 'yes'
-};
-
 let myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
+  this.read = read;
 }
 
-Book.prototype.info = function() {
-  return this.title + " by " + this.author + ", " + this.pages + " pages";
-}
+function addBookToLibrary() {
+  let title = document.getElementById("title").Value;
+  let author = document.getElementById("author").Value;
+  let pages = document.getElementById("pages").value;
+  let read = document.querySelector('input[name="read"]:checked').value; 
+read = read ? read.value : "Not read";
 
-function addBookToLibrary(title, author, pages) {
-  const newBook = new Book(title, author, pages);
+  let newBook = new Book(title, author, pages, read);
+
   myLibrary.push(newBook);
-}
 
-const array = []
-array.forEach(function (item, index)  {
-  console.log(item, index);
+  console.log("Book has been added to Library");
+}
+document.querySelector(".submit").addEventListener("click", function(event) {
+  addBookToLibrary();
 });
-
-function addBooksToLibrary(books) {
-  books.forEach(function(book) {
-    const newBook = new Book(book.title, book.author, book.pages, book.read);
-    myLibrary.push(newBook);
-  });
-}
-
-const booksToAdd = [book, book2, book3, book4];
-addBooksToLibrary(booksToAdd);
-
-const books = document.querySelectorAll('.book');
-
-books.forEach(function(book) {
-  const readButton = book.querySelector('.read-button');
-  const notReadButton = book.querySelector('.not-read-button');
-
-  readButton.addEventListener('click', function() {
-    readButton.classList.add('selected');
-    notReadButton.classList.remove('selected');
-  });
-
-  notReadButton.addEventListener('click', function() {
-    notReadButton.classList.add('selected');
-    readButton.classList.remove('selected');
-  });
-});
-
-function addBooksToDropdown(books) {
-  const dropdownContent = document.querySelector('.dropdown-content');
-  books.forEach(function(book) {
-    const bookDetails = document.createElement('div');
-    bookDetails.classList.add('book-details');
-    bookDetails.innerHTML = `<p>${book.title}</p>
-                              <p>Author: ${book.author}</p>
-                              <p>Pages: ${book.pages}</p>
-                              <p>Read: ${book.read}</p>`;
-    bookDetails.onclick = function() {
-      const bookTitle = book.title;
-      const bookAuthor = book.author;
-      const bookContainer = document.querySelector('#book-container');
-      bookContainer.innerHTML = `<div class="book-title-container">
-                                    <div class="name">${bookTitle} by ${bookAuthor}</div>
-                                    <button class="read-button">Read</button>
-                                    <button class="not-read-button">Not Read</button>
-                                  </div>`;
-    }
-    dropdownContent.appendChild(bookDetails);
-  });
-}
-
-addBooksToDropdown(myLibrary);
 
